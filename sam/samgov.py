8af9fbd33b266e89c75ebe870594750cf7767675
Dhruv Sood <dhruvsood27@gmail.com>
2021-06-09 11:24:19 -0700

    sending multiple requests per naics code

--- sam/samgov.py
+++ sam/samgov.py
@@ -1,31 +1,29 @@
 """
+README
+- make sure API key is private and not pushed to GitHub
 """
 import requests
 from pprint import pprint
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+import webbrowser
 import json
 import csv
 
 # required variables
-API_KEY = "3sX5w6MJhUWz2ks5lbCKqjk7X4c1xspeGr6xASfz"
-LIMIT = 10 # number of search results, we have a limit of 1000 requests per day
+API_KEY = "3sX5w6MJhUWz2ks5lbCKqjk7X4c1xspeGr6xASfz" # don't push this!
+LIMIT = 100 # number of search results, we have a limit of 1000 requests per day
 # posted dates - Date ranges must be 1 year apart (1 year maximum)
 FROM_DATE = (datetime.now() - relativedelta(months=3)).strftime("%m/%d/%Y")
 TO_DATE = (datetime.now() + relativedelta(months=7)).strftime("%m/%d/%Y")
 
 # optional variables
-PTYPE = 'o' # procurement type, 'o' = Soliciation
-DEPT = 'navy' # department name (deprecated)
-NAICS = '541330' # NAICS code, currently only accepting one response at a time, so we might need to run multiple requests
-# response deadlines 
-RDL_FROM = ''
-RDL_TO = ''
-
-# SET_ASIDE_TYPE
+NAICS = ['541330', '541519', '541611'] # array containing all naics codes
+RDL_FROM = (datetime.now() + relativedelta(months=3)).strftime("%m/%d/%Y") # response deadline from (+3 months)
+RDL_TO = (datetime.now() + relativedelta(months=23)).strftime("%m/%d/%Y") # response deadline to (+24 months)
 
 # creating request url
-response_link = ('https://api.sam.gov/prod/opportunities/v1/search?'
+req_url = ('https://api.sam.gov/prod/opportunities/v1/search?'
     + 'limit=' + str(LIMIT)
     + '&api_key=' + API_KEY 
     + '&postedFrom=' + FROM_DATE 
@@ -34,55 +32,43 @@ response_link = ('https://api.sam.gov/prod/opportunities/v1/search?'
 # using dictionary for optional variables
 optional = {
     "ptype": 'o',
-    "ncode": '541330',
+    "rdlfrom": FROM_DATE,
     "rdlto": TO_DATE
 }
 keys_list = list(optional)
 for i in range(len(optional)):
-    response_link += "&" + keys_list[i] + "=" + str(optional[keys_list[i]])
+    req_url += "&" + keys_list[i] + "=" + str(optional[keys_list[i]])
 
-"""
-workflow
-- get a bunch of requests
-- format the responses by "award date"
+reqs = []
+# create a request string for each naics code
+for i in range(len(NAICS)):
+    reqs.append(req_url + "&ncode" + NAICS[i]) 
 
-response parameters to check
-setAside
-setAsideDescription
-responseDeadLine
-active
-data.award.amount
-data.award.date
-"""
+# declare csv file to write to
+data_file = open("output.csv", "w+", newline='')
+data_file.truncate()
 
-print(response_link)
+# run each request
+for i in range(len(reqs)):
+    req_url = reqs[i]
 
-# get request
-response = requests.get(response_link)
-if (response == 200): # 200 - approved status code
-    print("good to go")
+    # opens the file in web browser
+    # webbrowser.open(req_url)
+    response = requests.get(req_url)
 
-# checking response parameters
-response_params = {
-    "data.award.awardee.name": "Lockheed Martin"
-}
+    # storing response
+    oppy_data = response.json()['opportunitiesData']
 
-response_params_keys = list(response_params)
-for i in range(len(response_params)):)
+    # writing to csv file
+    csv_writer = csv.writer(data_file)
 
-# storing/cleaning response
-oppy_data = response.json()['opportunitiesData']
+    count = 0
+    for data in oppy_data:
+        if count == 0:
+            header = data.keys()
+            csv_writer.writerow(header)
+            count += 1
+        csv_writer.writerow(data.values())
 
-data_file = open('output.csv', 'w', newline='')
-csv_writer = csv.writer(data_file)
-
-# writing out opportunities to csv file
-count = 0
-for data in oppy_data:
-    if count == 0:
-        header = data.keys()
-        csv_writer.writerow(header)
-        count += 1
-    csv_writer.writerow(data.values())
 
 data_file.close()
